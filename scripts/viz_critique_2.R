53ed8a3094db55bac3255a6378ab29abcb09bfa6
alexandralalor <allielalor@gmail.com>
2022-05-02 20:01:17 -0700

    viz critique and final project work, including naming for loop

--- scripts/viz_critique_2.R
+++ scripts/viz_critique_2.R
@@ -8,9 +8,6 @@
 # Figure 2 / UPDATE
 ##############################################################
 
-#load packages
-library(tidyverse)
-library(ggpubr)
 
 # code from...
 # https://zenodo.org/record/3833928#.Ym9XNNrMI2w
@@ -18,66 +15,66 @@ library(ggpubr)
 # description of Policies...
 # https://asanchez-tojar.github.io/code_in_ecology/supporting_information.html
 
+# The authors randomly sampled articles published in ecological journals for which code-sharing 
+# has been either mandatory or encouraged since June 2015 at the latest.
+# A total of 14 out of 96 (15%) ecological journals encouraged code-sharing policies
+# A random sample of 400 articles was taken from all the articles published in these
+# 14 journals. Articles were further screened for relevancy, leaving 346 studies in this review.
+
 # Based on the information collected, we scored the journals’ code-sharing policies as:
 #‘encouraged’ (publication of the code is explicitly encouraged, but not required), 
 #‘mandatory’ (code must be published together with the article), or
 #‘encouraged/mandatory’ (when the wording made it difficult to judge if code 
 # publication is encouraged or required).
 
+
+#load packages
+library(tidyverse)
+library(ggpubr)
+
 #read csv
 full.journal.info <- read_csv("data_raw/viz_critique/code_availability.csv")
 
-#group and summarize
-full.journal.info <- full.journal.info %>% 
-  mutate(Policy = ifelse(Policy == "Encouraged/Mandatory", "Encouraged", 
-                              ifelse(Policy == "Encouraged", "Encouraged", "Mandatory")))
-code_availability <- full.journal.info %>% 
-  group_by(Policy) %>%
-  summarize(total_comb = sum(total),
-            codepublished_comb = sum(codepublished),
-            percentage_comb = round((codepublished_comb/total_comb)*100))
+#mutate policy column, add count column, merge
+code_availability_1 <- full.journal.info %>% 
+  mutate(Policy_sort = ifelse(Policy == "Encouraged/Mandatory", "Encouraged_total", 
+                              ifelse(Policy == "Encouraged", "Encouraged_total", "Mandatory_total"))) %>% 
+  mutate(count = total) %>% 
+  mutate(Policy = "Total") %>% 
+  mutate(percentage = NA)
+
+code_availability_2 <- full.journal.info %>% 
+  mutate(Policy_sort = ifelse(Policy == "Encouraged/Mandatory", "Encouraged_published", 
+                         ifelse(Policy == "Encouraged", "Encouraged_published", "Mandatory_published"))) %>% 
+  mutate(count = codepublished) %>% 
+  mutate(Policy = ifelse(Policy_sort == "Encouraged_published", "Encouraged", "Mandatory"))
+
+code_availability <- rbind(code_availability_1,code_availability_2)
 
 
-class(code_availability$percentage_comb)
-
+#visualize
 code_availability %>%
-  ggdotchart(y = "Policy", x = "percentage_comb",
+  ggdotchart(x = "abbreviations", 
+             y = "count",
              color = "Policy",
-             palette = c("#00AFBB", "#E7B800"),
-             sorting = "descending",
-             group = "Policy",
-             scale_x_continuous(limits = c(0,30)))
-             xlab = "",
-             ylab = "Percentage (%) of articles publishing some code",
-             dot.size = 10,
-             label = paste0(round(code_availability$codepublished_comb,0)," / ",
-                            round(code_availability$total_comb,0)),
-             font.label = list(color = "black", size = 10,vjust = -2.5),
-             ggtheme = theme_pubr())
-
-
-code_availability %>%
-  ggdotchart(x = "Policy", y = "percentage_comb",
-             color = "Policy",
-             palette = c("#00AFBB", "#E7B800"),
+             palette = c("#00AFBB", "#E7B800", "#D3D3D3"),
              sorting = "descending",
              rotate = TRUE,
-             xlim = 40,
-             add = "segments",
              group = "Policy",
+             add = "segments",
+             title = "Ecology journals with code-sharing policies still have low availability of code",
              xlab = "",
-             ylab = "Percentage (%) of articles publishing some code",
-             dot.size = 10,
-             label = paste0(round(code_availability$codepublished_comb,0)," / ",
-                            round(code_availability$total_comb,0)),
-             font.label = list(color = "black", size = 10,vjust = -2.5),
+             ylab = "Number of articles with published code (color) of those reviewed (grey)",
+             dot.size = 8,
+             label = paste0(code_availability$percentage,"%"),
+             label.select = list(criteria = "Policy %in% c('Encouraged','Mandatory')"),
+             font.label = list(color = "black", size = 7,vjust = 0.5),
              ggtheme = theme_pubr())
 
-orientation = c("vertical", "horizontal", "reverse")
-?desc_statby
-?add
+
+
 # creating figure 2: ORIGINAL
-figure2 <- ggdotchart(full.journal.info, x = "abbreviations", y = "percentage",
+ggdotchart(full.journal.info, x = "abbreviations", y = "percentage",
                       color = "Policy",
                       palette = c("#00AFBB", "#E7B800", "#FC4E07"),
                       sorting = "descending",
@@ -92,4 +89,3 @@ figure2 <- ggdotchart(full.journal.info, x = "abbreviations", y = "percentage",
                       font.label = list(color = "black", size = 7,vjust = 0.5),
                       ggtheme = theme_pubr())
 
-figure2 <- ggpar(figure2,legend.title = "Code-sharing policy")
